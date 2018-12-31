7c2fe463a023ec62241b45f2f50ce6b17cec24f0
willgithub <524869004@qq.com>
2018-12-31 12:02:23 +0800

    Update git-install.sh

--- git-install.sh
+++ git-install.sh
@@ -1,29 +1,25 @@
+#!/bin/bash
+#卸载旧版本git
+yum remove git
+
 #安装编译环境依赖包
-yum install -y gcc gcc-c++ autoconf make automake wget
+yum install -y gcc gcc-c++
 
 #安装Git编译过程需要的依赖关系库
-yum install -y perl curl-devel expat-devel gettext-devel openssl-devel zlib-devel
-yum install -y cpio texinfo sgml2xml openjade perl-ExtUtils-MakeMaker
-
-#支持更多文档格式（如 doc, html, info）
-yum install -y asciidoc xmlto docbook2x
-
-#解决执行make all doc info时报:/bin/sh: line 1: docbook2x-texi: command not found
-ln -s /usr/bin/db2x_docbook2texi /usr/bin/docbook2x-texi
+yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel
 
 #下载git源码包，以git-2.20.1为例
 wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.20.1.tar.gz
 
 #编译安装git
 tar -zxvf git-2.20.1.tar.gz
-cd git-2.20.1 && make configure && ./configure --prefix=/usr/local/git
-make all doc info
-make install install-doc install-html install-info
+cd git-2.20.1 
+./configure --prefix=/usr/local/git
+make && make install
 
-#配置系统环境
+# 配置环境变量
 cat >> /etc/profile << EOF
-export GIT_HOME=/usr/local/git
-export PATH=${GIT_HOME}/bin:${PATH}
+export PATH=/usr/local/git/bin:$PATH
 EOF
 source /etc/profile
 
@@ -33,4 +29,4 @@ echo 'source "/home/git-completion.bash"' >> $HOME/.bashrc
 source $HOME/.bashrc
 
 #查看git版本
-git –version
+git --version
