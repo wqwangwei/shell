b0b57dc643f4e00697dafa5de52420003a5eb947
will <524869004@qq.com>
2019-01-16 14:29:16 +0800

    Update centos7_init.sh

--- centos7_init.sh
+++ centos7_init.sh
@@ -58,6 +58,7 @@ rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
 yum --enablerepo=elrepo-kernel install -y kernel-ml
 grub2-set-default 0
 grub2-mkconfig -o /boot/grub2/grub.cfg
+yum --exclude=kernel* update -y
 }
 
 #执行脚本
