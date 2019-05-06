5fcca95896d00dedf6ec4aa26c6451080c9af07d
willgithub <524869004@qq.com>
2019-05-06 22:41:04 +0800

    Update pull_k8s_images.sh

--- pull_k8s_images.sh
+++ pull_k8s_images.sh
@@ -1,54 +1,22 @@
+cat > pull_dockerhub.sh << EOF
 #!/bin/bash
-#本脚本将拉取以下9个images
-#kube-proxy-amd64:v1.13.0
-#kube-controller-manager-amd64:v1.13.0
-#kube-scheduler-amd64:v1.13.0
-#kube-apiserver-amd64:v1.13.0
-#pause-amd64:3.1
-#coredns:1.2.6
-#etcd-amd64:3.2.24
-#kubernetes-dashboard-amd64:v1.10.0
-#flannel:v0.10.0-amd64
 
-set -e
+##########################################################
+cat > img-list.txt << EOF
+gcr.io/heptio-images/sonobuoy:v0.13
+gcr.io/heptio-images/kube-conformance:latest
+gcr.io/heptio-images/scanner-forwarder:v0.0.4
+gcr.io/heptio-images/namespace-deleter:v0.0.1
+EOF
+##########################################################
 
-#运行kubeadm config images list确认指定版本
-K8S_VERSION=v1.13.0
-ETCD_VERSION=3.2.24
-DASHBOARD_VERSION=v1.10.0
-FLANNEL_VERSION=v0.10.0-amd64
-DNS_VERSION=1.2.6
-PAUSE_VERSION=3.1
-
-## 拉取images
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:$K8S_VERSION
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager-amd64:$K8S_VERSION
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler-amd64:$K8S_VERSION
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64:$K8S_VERSION
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/etcd-amd64:$ETCD_VERSION
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/pause-amd64:$PAUSE_VERSION
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$DNS_VERSION
-docker pull registry.cn-hangzhou.aliyuncs.com/kubernetes_containers/flannel:$FLANNEL_VERSION
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kubernetes-dashboard-amd64:$DASHBOARD_VERSION
-
-## 修改tag
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:$K8S_VERSION k8s.gcr.io/kube-apiserver-amd64:$K8S_VERSION
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager-amd64:$K8S_VERSION k8s.gcr.io/kube-controller-manager-amd64:$K8S_VERSION
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler-amd64:$K8S_VERSION k8s.gcr.io/kube-scheduler-amd64:$K8S_VERSION
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64:$K8S_VERSION k8s.gcr.io/kube-proxy-amd64:$K8S_VERSION
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/etcd-amd64:$ETCD_VERSION k8s.gcr.io/etcd-amd64:$ETCD_VERSION
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/pause-amd64:$PAUSE_VERSION k8s.gcr.io/pause-amd64:$PAUSE_VERSION
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$DNS_VERSION k8s.gcr.io/coredns:$DNS_VERSION
-docker tag registry.cn-hangzhou.aliyuncs.com/kubernetes_containers/flannel:$FLANNEL_VERSION quay.io/coreos/flannel:$FLANNEL_VERSION
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kubernetes-dashboard-amd64:$DASHBOARD_VERSION k8s.gcr.io/kubernetes-dashboard-amd64:$DASHBOARD_VERSION
-
-## 删除原镜像
-docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:$K8S_VERSION
-docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager-amd64:$K8S_VERSION
-docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler-amd64:$K8S_VERSION
-docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64:$K8S_VERSION
-docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/etcd-amd64:$ETCD_VERSION
-docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/pause-amd64:$PAUSE_VERSION
-docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$DNS_VERSION
-docker rmi registry.cn-hangzhou.aliyuncs.com/kubernetes_containers/flannel:$FLANNEL_VERSION
-docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kubernetes-dashboard-amd64:$DASHBOARD_VERSION
+DOCKER_HUB=willdockerhub
+for image in $(cat img-list.txt)
+do
+	imagename=$(echo $image | awk -F '/' '{print $NF}')
+	docker pull $DOCKER_HUB/$imagename
+	docker tag $DOCKER_HUB/$imagename $image
+	docker rmi $DOCKER_HUB/$imagename
+done
+EOF
+sh pull_dockerhub.sh
