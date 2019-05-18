ee210a856b47ecdf5a95d88a4405b19b6de53a36
willgithub <524869004@qq.com>
2019-05-18 20:35:36 +0800

    Update docker_run_ssr.sh

--- shadowsocks/docker_run_ssr.sh
+++ shadowsocks/docker_run_ssr.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 docker stop ssserver
 docker rm ssserver
 echo -n "Enter your passwd:"
 read  passwd
-#docker run -dt --name ssserver -p 6443:6443 mritd/shadowsocks -s "-s 0.0.0.0 -p 6443 -m chacha20-ietf-poly1305 -k password"
+#docker run -dt --name ssserver -p 6443:6443 mritd/shadowsocks -s "-s 0.0.0.0 -p 6443 -m chacha20-ietf-poly1305 -k $password"
 docker run -dt --name ssserver --restart always -p 6443:6443 -p 6500:6500/udp mritd/shadowsocks -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m chacha20-ietf-poly1305 -k $passwd" -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2"
