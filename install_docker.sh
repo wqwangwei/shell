6534aeca1fe64685e2a748ef245f86df3cbb0c96
willgithub <524869004@qq.com>
2019-05-18 20:28:29 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -5,10 +5,6 @@ set -e
 #指定安装的docker版本
 docker_version=docker-ce
 
-#卸载旧版本docker
-yum remove -y docker
-yum remove -y docker-ce
-
 #安装依赖软件包
 yum install -y yum-utils \
 	device-mapper-persistent-data \
@@ -19,6 +15,7 @@ yum install -y yum-utils \
 yum-config-manager \
     --add-repo \
     http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+
 #安装docker
 yum install -y $docker_version
 
@@ -31,9 +28,6 @@ tee /etc/docker/daemon.json <<-'EOF'
   "registry-mirrors": ["https://uyah70su.mirror.aliyuncs.com"]
 }
 EOF
-
-##配置daocloud镜像加速
-#curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io >> /dev/null
 
 #重启docker服务
 systemctl daemon-reload && systemctl restart docker.service
