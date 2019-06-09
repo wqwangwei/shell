b966eadb537a150471f9f3dc3df993cb7ff5a732
willgithub <524869004@qq.com>
2019-06-09 13:07:42 +0800

    Create google_vm_init.sh

---
+++ google_vm_init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#password=<>
+(echo "$password";sleep 1;echo "$password") | sudo passwd root > /dev/null
+sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
