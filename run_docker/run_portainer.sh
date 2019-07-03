d0dd9305f570f5ed7392983e3f347c0216765428
willgithub <524869004@qq.com>
2019-07-04 00:35:39 +0800

    Update run_portainer.sh

--- run_docker/run_portainer.sh
+++ run_docker/run_portainer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker volume create portainer_data
-docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker run -itd --name=myportainer --restart=always -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
