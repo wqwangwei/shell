3875eec13c5c992b080a27777374f28cac6b0aaf
willgithub <524869004@qq.com>
2019-08-03 16:11:30 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,6 +2,7 @@
 #install the latest version of docker-ce
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh --mirror Aliyun
+mkdir -p /etc/docker
 tee /etc/docker/daemon.json <<-'EOF'
 {
   "registry-mirrors": ["https://uyah70su.mirror.aliyuncs.com"]
