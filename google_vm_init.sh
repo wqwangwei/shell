15555dac67fa2eb24525913c803dcb64a11b9275
willgithub <524869004@qq.com>
2019-09-06 17:12:00 +0800

    Update google_vm_init.sh

--- google_vm_init.sh
+++ google_vm_init.sh
@@ -4,3 +4,5 @@
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
+
+curl -sSL https://raw.githubusercontent.com/willzhang/shell/master/install_docker.sh | sh
