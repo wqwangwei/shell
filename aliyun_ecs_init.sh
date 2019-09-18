0fe466dd0ac8f3f97642e07baf7a963ef231c22f
willgithub <524869004@qq.com>
2019-09-19 00:07:12 +0800

    Update aliyun_ecs_init.sh

--- aliyun_ecs_init.sh
+++ aliyun_ecs_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #set root password
-echo "123456" | passwd --stdin root >/dev/null 2>&1
+echo "root:123456" | sudo chpasswd >/dev/null 2>&1
 
 #enable password authentication
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
