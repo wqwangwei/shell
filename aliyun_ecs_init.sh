57a70adf873724c1017d6f683320d04fee5ebc94
willgithub <524869004@qq.com>
2019-09-18 10:49:13 +0800

    Update aliyun_ecs_init.sh

--- aliyun_ecs_init.sh
+++ aliyun_ecs_init.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-#password=<>
-(echo "$password";sleep 1;echo "$password") | sudo passwd root > /dev/null
+#set root password
+#password=< your password >
+echo "$password" | passwd --stdin root >/dev/null 2>&1
+
+#enable password authentication
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-systemctl restart sshd
+systemctl reload sshd
 
+#install docker
 curl -sSL https://raw.githubusercontent.com/willzhang/shell/master/install_docker.sh | sh
