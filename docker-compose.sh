4ac8f37ccb435c328ad0d90406dca05f5598fb52
willgithub <524869004@qq.com>
2019-11-20 11:15:15 +0800

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-version=1.24.1
-curl -L https://get.daocloud.io/docker/compose/releases/download/${version}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -s https://api.github.com/repos/docker/compose/releases/latest \
+  | grep browser_download_url \
+  | grep Linux-x86_64 \
+  | grep -v sha256 \
+  | cut -d '"' -f 4 \
+  | xargs curl -Lo /usr/local/bin/docker-compose
+
 chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-curl -L https://raw.githubusercontent.com/docker/compose/${version}/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
