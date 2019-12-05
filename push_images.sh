5f7c07eef2c54e7fb11963e2db2e9c19b38789de
willgithub <524869004@qq.com>
2019-12-05 17:55:05 +0800

    Update push_images.sh

--- push_images.sh
+++ push_images.sh
@@ -1,13 +1,59 @@
 #!/bin/bash
-#destination registry you want push
-registry=registry.cn-hangzhou.aliyuncs.com/aliwill/
+script_dir=$(dirname $(readlink -f $0 ))
 
-#perpare imagepath.txt at current dir
-for imagepath in $(cat imagepath.txt)
+image_list=$(cat $script_dir/image_list.txt)
+
+#destination registry to push
+registry=192.168.93.40/sonobuoy
+
+pull_image(){
+for image in $image_list
 do
-imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
-docker pull $imagepath
-docker tag $imagepath $registry$imagename
-docker push $registry$imagename
-docker rmi $imagepath
+  docker pull $image
 done
+}
+
+save_image(){
+if [ ! -d $script_dir/images ];then mkdir $script_dir/images;fi
+
+save_dir=$script_dir/images
+
+for image in $image_list
+do
+  image_name=${image##*/}
+  docker save $image -o $save_dir/${image_name}.tar
+done
+}
+
+load_image(){
+save_dir=$script_dir/images
+
+for image in $image_list
+do
+  image_name=${image##*/}
+  docker load -i $save_dir/${image_name}.tar
+done
+}
+
+retag_image(){
+for image in $image_list
+do
+
+  image_name=${image##*/}
+  docker tag $image $registry/$image_name
+done
+}
+
+push_image(){
+for imagepath in $(cat image_list.txt)
+do
+  image_name=${image##*/}
+  docker push $registry/$image_name
+done
+}
+
+pull_image
+save_image
+load_image
+retag_image
+push_image
