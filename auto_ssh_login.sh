d1ca55cd98883796c9f144b52b99ad52a1287de7
willgithub <524869004@qq.com>
2020-03-03 09:23:33 +0800

    Update auto_ssh_login.sh

--- auto_ssh_login.sh
+++ auto_ssh_login.sh
@@ -1,12 +1,11 @@
 #定义主机IP、用户名及密码
 cat > hosts.list << EOF
-#!/bin/bash
-192.168.93.31 root 123456
-192.168.93.32 root 123456
+172.20.8.2 root 123456
+172.20.8.3 root 123456
 EOF
 
 #本机对所有节点ssh免密
-cat > auto_ssh.sh << EOF
+cat auto_ssh.sh
 #!/bin/bash
 rpm -qa | grep sshpass >> /dev/null; if [ $? -ne 0 ]; then yum install -y sshpass; fi
 if [ ! -f "/root/.ssh/id_rsa.pub" ]; then echo -e 'y\n' | ssh-keygen -q -t rsa -b 2048 -N "" -f /root/.ssh/id_rsa; fi
@@ -15,6 +14,5 @@ do
   echo -e "\033[32m >>> ${IP} \033[0m"
   sshpass -p $PASS ssh-copy-id -o StrictHostKeyChecking=no $USER@$IP
 done < hosts.list
-EOF
 
 sh auto_ssh.sh
