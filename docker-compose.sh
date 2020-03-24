d5c140ce62169a6f5cd350d87a715a75a52341a1
willgithub <524869004@qq.com>
2020-03-24 11:18:49 +0800

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-curl -s https://api.github.com/repos/docker/compose/releases/latest \
-  | grep browser_download_url \
-  | grep Linux-x86_64 \
-  | grep -v sha256 \
-  | cut -d '"' -f 4 \
-  | xargs curl -Lo /usr/local/bin/docker-compose
-
+version=1.25.4
+curl -L https://get.daocloud.io/docker/compose/releases/download/${version}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
