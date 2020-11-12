40f49163bd9983ed59f3711eeda4bf830f4a48e2
wqwangwei <wqwangwei@gmail.com>
2020-11-12 09:14:19 +0800

    Update docker-ce.sh

--- docker-ce.sh
+++ docker-ce.sh
@@ -2,7 +2,8 @@
 
 #install latest version of docker-ce
 curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
-
+sed -e 's/7/8/g' /etc/yum.repos.d/docker-ce.repo
+yum makecache fast
 #Configuration Mirror Acceleration
 mkdir -p /etc/docker
 tee /etc/docker/daemon.json <<-'EOF'
